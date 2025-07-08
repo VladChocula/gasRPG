5950d79f9e8bee208196554f31059f34d1c35220
SneakersSO <sr.oliveira86@gmail.com>
2025-07-08 19:13:15 -0400

    ProjectileBase
    -Created custom projectile actor
    -Set default values for projectile behavior
    --created a BP_firebolt based on the new projectile actor for the project

---
+++ Source/SUARPG/Public/Actor/AuraProjectile.h
@@ -0,0 +1,36 @@
+// Steven Oliveira
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AuraProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class SUARPG_API AAuraProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	AAuraProjectile();
+
+	UPROPERTY(VisibleAnywhere, Category = "Projectile")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	
+	UPROPERTY(VisibleAnywhere, Category = "Projectile")
+	TObjectPtr<USphereComponent> Sphere;
+
+};
